fef4d14b8ee6a40d33b53447c50a5ee2bdb211e5
haardik madaan <haardik@kali.haardik>
2025-04-16 21:55:49 +0530

    Solved count three, count ones

---
+++ count_ones.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int dp[10][2][10];
+
+int solve(string str, int idx, bool tight, int cnt){
+    if(idx == str.size()){
+        return cnt;
+    }
+
+    if(dp[idx][tight][cnt]!=-1) return dp[idx][tight][cnt];
+
+    int limit = (tight == 1 ? str[idx]-'0' : 9);
+    int ans = 0;
+
+    for (int i = 0;i<=limit;i++){
+        ans += solve(str,idx+1,tight && (i == str[idx]-'0'),cnt + (i == 1 ? 1 : 0));
+    }
+
+    return dp[idx][tight][cnt] = ans;
+
+}
+
+int main(){
+    int n;
+    cin>>n;
+    string num = to_string(n);
+    memset(dp,-1,sizeof(dp));
+    int ans = solve(num,0,1,0);
+    cout<<ans;
+}
---
+++ count_three.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+int dp[10][2][10];
+
+int solve(string str, int idx, int tight,int cnt){
+    if(idx == str.size()){
+        return cnt;
+    }
+
+    if(dp[idx][tight][cnt] != -1){
+        return dp[idx][tight][cnt];
+    }
+
+    int limit = (tight == 1 ? str[idx] - '0': 9);
+    int ans = 0;
+
+    
+    for (int i = 0;i<=limit;i++){
+        ans += solve(str, idx + 1, tight && (i == str[idx] - '0'), cnt + (i == 3 ? 1:0));
+    }
+    return dp[idx][tight][cnt] = ans;
+}
+
+int main(){
+    int l;
+    int r;
+    cin>>l>>r;
+    string left = to_string(l-1);
+    string right = to_string(r);
+
+    memset(dp,-1,sizeof(dp));
+    int leftans = solve(left, 0, 1 , 0);
+    memset(dp,-1,sizeof(dp));
+    int rightans = solve(right,0,1,0);
+
+    cout<<rightans - leftans;
+}
